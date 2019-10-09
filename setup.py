9c9fd6b5ef31e97c38e977aec3d3192ed0653766
David N. Mashburn <david.n.mashburn@gmail.com>
2019-10-09 12:03:54 -0400

    Versioneer-ize

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
+import versioneer
+
 from distutils.core import setup
 
+
 setup(
-    name='cmpGen',
-    version='0.1.2', # final version, now deprecated
-    author='David N. Mashburn',
-    author_email='david.n.mashburn@gmail.com',
-    packages=['cmpGen'],
+    name="cmpGen",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="David N. Mashburn",
+    author_email="david.n.mashburn@gmail.com",
+    packages=["cmpGen"],
     scripts=[],
-    url='http://pypi.python.org/pypi/cmpGen/',
-    license='LICENSE.txt',
-    description='cmpGen is deprecated; it is maintained here for legacy purposes',
-    long_description=open('README.rst').read(),
+    url="http://pypi.python.org/pypi/cmpGen/",
+    license="LICENSE.txt",
+    description="cmpGen is deprecated; it is maintained here for legacy purposes",
+    long_description=open("README.rst").read(),
     install_requires=[],
 )
